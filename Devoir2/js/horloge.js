f6dbaf07b7cd498cfff8befdfd635a43ff47fbb6
Lei Jinmin <1697899@cegepoutaouais.qc.ca>
2017-05-11 10:17:15 -0400

    Remove the application of jquery library as demanded

--- Devoir2/js/horloge.js
+++ Devoir2/js/horloge.js
@@ -1,29 +1,23 @@
 ﻿// Préparation des dossiers necessaires
 (function() {
-  // Ajouter jquery dans le contexte
-  var script = document.createElement('script'),
-      sibling = document.getElementsByTagName('script')[0]; 
-      parent = sibling.parentElement;
-      script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js';
-      script.type = 'text/javascript';
-      parent.insertBefore(script,sibling); 
-
       window.onload = function(){
         
          class Horloge {
 
             constructor(json) {
+              this.audioElement = document.createElement("audio");
+              this.audioElement.loop = true;
+              this.audioElement.autoplay = true;
+              document.getElementsByTagName("body")[0].appendChild(this.audioElement);
+
               this.action = json.son;
-              this.element = $("#"+json.id);
-
-              $("body").append("<audio loop autoplay></audio>");
-
+              this.heureElement = document.getElementById(json.id);
               this.alarme = json.alarme;
 
               this.on = function(eventType,fn) {
                 if(eventType === "alarme" && fn !== undefined)
                   fn();
-              };
+              };              
             }
             // Retourner l'heure à chaine et définir l'heure courrante
             get HeureCourrante() {
@@ -51,7 +45,7 @@
 
             // Afficher l'heure et faire l'alarme
             AfficherEtAlarmer() {
-              this.element.text(this.HeureCourrante);
+              this.heureElement.innerText = this.HeureCourrante;
               if (this.heureAlarme !== undefined && 
                   this.HeureAChaine(this.heureCourrante) === this.HeureAChaine(this.heureAlarme))
                 this.on("alarme", this.faireAlarme(this.action));                  
@@ -66,21 +60,17 @@
 
             jouerAlarme() {
               var index = Math.floor(Math.random() * this.alarme.length); 
-              $("audio").attr("src",this.alarme[index].path);
+              this.audioElement.src = this.alarme[index].path; 
             }
 
             Arreter() { 
-              $("audio").attr("src",""); 
+              this.audioElement.src = ""; 
               this.heureAlarme = undefined;
             }
 
           }
          
          // Lire donnée de musique. 
-         // Mais pour tirer les données sauvegardées sur le disque, 
-         // on a besoin d'exécuter le programme sur le serveur.
-         // $.getJSON( "data2.json", function(json) {  
-
             (function(json) {
               var horloge = new Horloge(json);
                        
@@ -88,28 +78,33 @@
                 horloge.AfficherEtAlarmer();
               },1000);
          
-              $('#btn_ajouterAlarme').click(function(){
-                if($(this).html() === "Ajouter une alarme")
+              
+              document.getElementById("btn_ajouterAlarme").addEventListener("click", function(){
+                if(this.innerText === "Ajouter une alarme")
                 {
                               
-                  heure = $("#select_alarmeHeures option:selected").text();
-                  minute = $("#select_alarmeMinutes option:selected").text();
-                  seconde = $("#select_alarmeSecondes option:selected").text();
+                  var e = document.getElementById("select_alarmeHeures");
+                  heure =  e.options[e.selectedIndex].value
+                  e = document.getElementById("select_alarmeMinutes");
+                  minute = e.options[e.selectedIndex].value
+                  e = document.getElementById("select_alarmeSecondes");
+                  seconde =  e.options[e.selectedIndex].value;
                   
                   if(heure>0 || minute>0 ||seconde>0) {
-                      $("#alarme h1").html(horloge.ProgrammerNouvelleAlarme(parseInt(heure),
+                    document.querySelector("#alarme h1").innerText = horloge.ProgrammerNouvelleAlarme(parseInt(heure),
                                                                             parseInt(minute),
-                                                                            parseInt(seconde)));
-                      $(this).html("Arrêter");
+                                                                            parseInt(seconde));
+
+                    this.innerText = "Arrêter";
                    }
                   else 
                     alert("Choisir le temps à faire l'alarme.");
                 }
                 else { 
                   horloge.Arreter();
-                  $(this).html("Ajouter une alarme");
+                  this.innerText = "Ajouter une alarme";
                 }
-              });// end $('#btn_ajouterAlarme').click(...)
+              });// end  document.getElementById("btn_ajouterAlarme").addEventListener(...)
           })({
                 "son":true,
                 "id":"span_horloge",
@@ -121,8 +116,6 @@
                   {"nom":"piano_beat_mix", "path":"mp3/piano_beat_mix.mp3"}
                 ]
             }); // end function(json)
-        
-        //  });// end $.getJSON(...)   
 
        }; // end window.onload
         
